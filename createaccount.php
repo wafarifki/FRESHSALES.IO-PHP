f44025df97da69f9cbb28c6503018aff4dcb12c7
Wafa Rifqi Anafin <71179459+wafarifki@users.noreply.github.com>
2020-10-12 15:37:56 +0700

    Update createaccount.php

--- createaccount.php
+++ createaccount.php
@@ -4,9 +4,11 @@
 <title>FreshSales.IO PHP Sample Script</title>
 </head>
 <body>  
+  
 <!--Created By Wafa Rifqi Anafin-->
 <!--Use this for learn-->
 <!--Give issues if you want to get help from me-->
+  
 <h2>FreshSales.IO PHP Sample Script</h2>
 <p>Create Account from your website and integration to your freshsales.io Account dashoard</p><br>
 <form method="post" action="proccesaccount.php">  
